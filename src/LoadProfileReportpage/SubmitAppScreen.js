f455c35b3303e9f9dfbc44aab626f654e77b33c1
sravya539 <93430745+sravya539@users.noreply.github.com>
2022-01-19 23:39:12 -0800

    commit

--- src/LoadProfileReportpage/SubmitAppScreen.js
+++ src/LoadProfileReportpage/SubmitAppScreen.js
@@ -5,7 +5,8 @@ import DeleteIcon from "@material-ui/icons/Delete";
 import SearchIcon from "@material-ui/icons/Search";
 import SaveIcon from "@material-ui/icons/Save";
 import { Button } from "@material-ui/core";
-import { AppBar } from "@mui/material";
+import TSSPDCL from "../assets/TSSPDCL.jpg"
+import { Grid,Typography } from "@mui/material";
 
 export default function MaterialChart() {
   const [dataStore, setDataStore] = useState([
@@ -14,16 +15,42 @@ export default function MaterialChart() {
 
   return (
     <div className="MaterialChart">
-      <AppBar
-    title={<img src="C:\Users\Sravya\Downloads\TSSPDCL"/>}
-  />
-      <h1>TELANGANA STATE SOUTHERN POWER DISTRIBUTION COMPANY LIMITED</h1>
+      <br />
+       <Grid container direction="row" justifyContent="center" alignContent="center" >
+       <Grid item xs={2}>   
+        <img src={TSSPDCL} width="100px" />
+      </Grid>
+      <h2>TELANGANA STATE SOUTHERN POWER DISTRIBUTION COMPANY LIMITED</h2>
+      </Grid> 
       <a
         target="_blank"
         href="https://smartdevpreneur.com/intro-to-material-table-for-react/"
       >
         Click here for an intro to material-table
-      </a>
+      </a>   
+      < br />
+       <Grid container direction="row">
+       <Grid item xs={3}>
+          <Typography>
+           SubStation:
+          </Typography>   
+          </Grid>   
+          <Grid item xs={3}>     
+          <Typography>
+           Feeder:
+          </Typography> 
+          </Grid>  
+          <Grid item xs={3}>      
+          <Typography>
+           DTR:
+          </Typography>
+          </Grid>
+          <Grid item xs={3}>   
+          <Typography>
+           Meter:
+          </Typography>  
+          </Grid>
+          </Grid>
       <div style={{ maxWidth: "100%", paddingTop: "12px" }}>
         <MaterialTable
           columns={[
@@ -42,8 +69,56 @@ export default function MaterialChart() {
               }
             },
             {
-              title: "Age",
-              field: "age",
+              title: "Current(Amp)",
+              field: "Current(Amp)",
+              type: "numeric",
+              headerStyle: {
+                backgroundColor: "blue"
+              }
+            },
+            {
+              title: "Voltage(V)",
+              field: "Voltage(V)",
+              type: "numeric",
+              headerStyle: {
+                backgroundColor: "blue"
+              }
+            },
+            {
+              title: "Block Energy",
+              field: "Block Energy",
+              type: "numeric",
+              headerStyle: {
+                backgroundColor: "blue"
+              }
+            },
+            {
+              title: "Block Energy (KVARH)",
+              field: "Block Energy (KVARH)",
+              type: "numeric",
+              headerStyle: {
+                backgroundColor: "blue"
+              }
+            },
+            {
+              title: "Cumulative Energy",
+              field: "Cumulative Energy",
+              type: "numeric",
+              headerStyle: {
+                backgroundColor: "blue"
+              }
+            },
+            {
+              title: "Cumulative Energy (KVARH)",
+              field: "Cumulative Energy (KVARH)",
+              type: "numeric",
+              headerStyle: {
+                backgroundColor: "blue"
+              }
+            },
+            {
+              title: "Reactive Energy",
+              field: "Reactive Energy",
               type: "numeric",
               headerStyle: {
                 backgroundColor: "blue"
@@ -51,19 +126,13 @@ export default function MaterialChart() {
             }
           ]}
           data={dataStore}
-          title="Material-Table Demo"
+          title="TSSPDCL Data"
           icons={{
             Clear: (props) => <DeleteIcon />,
             Search: (props) => <SearchIcon />,
             ResetSearch: (props) => <DeleteIcon />
           }}
-          actions={[
-            {
-              icon: () => <SaveIcon />,
-              tooltip: "Save User",
-              onClick: (event, rowData) => alert("You saved " + rowData.name)
-            }
-          ]}
+         
           components={{
             Action: (props) => (
               <Button
