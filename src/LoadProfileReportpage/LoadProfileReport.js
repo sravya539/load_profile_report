cf9887df02dbe8c73d514a26151a9455b67b03be
sravya539 <93430745+sravya539@users.noreply.github.com>
2022-01-25 04:12:14 -0800

    commit

--- src/LoadProfileReportpage/LoadProfileReport.js
+++ src/LoadProfileReportpage/LoadProfileReport.js
@@ -1,17 +1,38 @@
 import React from 'react';
-import { Stack, Button, CssBaseline, AppBar, Toolbar, Typography, Box, TextField, Grid } from '@mui/material';
+import { Stack, Button, CssBaseline, AppBar, Toolbar, Typography, Box, TextField, Grid ,MenuItem} from '@mui/material';
 import Home from '@mui/icons-material/Home';
 import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
-import { DTR, Feeder, Section, SubStation } from './Dropdown';
+import { DTR } from './Dropdown';
 import { v4 as uuidv4 } from 'uuid';
 import { useState } from 'react';
 import { Link } from "react-router-dom";
 import ResponsiveDateRangePicker from './DateDetails';
 import MaterialChart from './SubmitAppScreen';
-
+import Sections from '../utils/Sections.json'
+import SubStations from '../utils/SubStation.json'
+import Feeder from '../utils/Feeder.json'
 
 export default function SelectTextFields() {
 
+  const [ section, setSection ] = useState([]);
+  const [ SubStation, setSubStation ] = useState([]); 
+  const [ newFeeder, setnewFeeder ] = useState([]);
+
+
+
+  const onSections = (e) => {
+		console.log(e.target.value);
+		const substas = SubStations.filter((substa) => {
+			return substa.sec_name === e.target.value;
+		});
+		console.log(substas);
+		setSection(e.target.value);
+		setSubStation(substas);
+	};
+	const onSubStation = (e) => {
+		console.log(e.target.value);
+		setnewFeeder(e.target.value);
+	};
   const initialValues = {
     Sections: '',
     SubStation: '',
@@ -73,40 +94,39 @@ export default function SelectTextFields() {
       <Box sx={{ display: 'flex', p: 1, m: 1, bgcolor: 'background.paper', flexDirection: 'row', }}>
       </Box>
       <Grid container direction="row" justifyContent="center" alignContent="center" >
-        <TextField
-          id="outlined-select-currency-native"
-          name="Sections"
-          value={values.Sections}
-          onChange={handleInputChange}
-          select
-          label="Sections:"
-          SelectProps={{
-            native: true,
-          }}
-        >
-          {Section.map((option) => (
-            <option key={option.value} value={option.label}>
-              {option.label}
-            </option>
-          ))}
+
+      <TextField
+							select
+							fullWidth
+							variant="outlined"
+							color="primary"
+							label="section"
+							onChange={onSections}
+							value={section}
+						>
+          	{section.map((sec) => (
+							<MenuItem value={sec.name} key={sec.sec_id}>
+									{sec.name}
+								</MenuItem>
+							))}
         </TextField>
         <TextField
           id="outlined-select-currency-native"
+          onChange={onSubStation}
           select
           required
           label="Sub Station:"
+          value={newFeeder}
           SelectProps={{
             native: true,
-          }}
-          name="SubStation"
-          value={values.SubStation}
-          onChange={handleInputChange}
+          }}          
         >
-          {SubStation.map((option) => (
-            <option key={option.value} value={option.label}>
-              {option.label}
-            </option>
-          ))}
+          {SubStation.map((substa) => (
+								<MenuItem value={substa.name} key={substa.substa_id}>
+									{substa.name}
+								</MenuItem>
+							))}
+          ))
         </TextField>
       </Grid>
       <Grid container direction="row" justifyContent="center" alignContent="center" >
--- src/LoadProfileReportpage/SubmitAppScreen.js
+++ src/LoadProfileReportpage/SubmitAppScreen.js
@@ -6,7 +6,8 @@ import SearchIcon from "@material-ui/icons/Search";
 import SaveIcon from "@material-ui/icons/Save";
 import { Button } from "@material-ui/core";
 import TSSPDCL from "../assets/TSSPDCL.jpg"
-import { Grid,Typography } from "@mui/material";
+import { Grid, Typography } from "@mui/material";
+
 
 export default function MaterialChart() {
   const [dataStore, setDataStore] = useState([
@@ -16,41 +17,42 @@ export default function MaterialChart() {
   return (
     <div className="MaterialChart">
       <br />
-       <Grid container direction="row" justifyContent="center" alignContent="center" >
-       <Grid item xs={2}>   
-        <img src={TSSPDCL} width="100px" />
+      <Grid container direction="row" justifyContent="center" alignContent="center" >
+        <Grid item xs={2}>
+          <img src={TSSPDCL} width="100px" />
+        </Grid>
+        <h2>TELANGANA STATE SOUTHERN POWER DISTRIBUTION COMPANY LIMITED</h2>
       </Grid>
-      <h2>TELANGANA STATE SOUTHERN POWER DISTRIBUTION COMPANY LIMITED</h2>
-      </Grid> 
       <a
         target="_blank"
         href="https://smartdevpreneur.com/intro-to-material-table-for-react/"
       >
         Click here for an intro to material-table
-      </a>   
-      < br />
-       <Grid container direction="row">
-       <Grid item xs={3}>
+      </a>
+      <br />
+      <br />
+      <Grid container direction="row">
+        <Grid item xs={3}>
           <Typography>
-           SubStation:
-          </Typography>   
-          </Grid>   
-          <Grid item xs={3}>     
-          <Typography>
-           Feeder:
-          </Typography> 
-          </Grid>  
-          <Grid item xs={3}>      
-          <Typography>
-           DTR:
+            SubStation:
           </Typography>
-          </Grid>
-          <Grid item xs={3}>   
+        </Grid>
+        <Grid item xs={3}>
           <Typography>
-           Meter:
-          </Typography>  
-          </Grid>
-          </Grid>
+            Feeder:
+          </Typography>
+        </Grid>
+        <Grid item xs={3}>
+          <Typography>
+            DTR:
+          </Typography>
+        </Grid>
+        <Grid item xs={3}>
+          <Typography>
+            Meter:
+          </Typography>
+        </Grid>
+      </Grid>
       <div style={{ maxWidth: "100%", paddingTop: "12px" }}>
         <MaterialTable
           columns={[
@@ -125,6 +127,7 @@ export default function MaterialChart() {
               }
             }
           ]}
+         
           data={dataStore}
           title="TSSPDCL Data"
           icons={{
@@ -132,7 +135,7 @@ export default function MaterialChart() {
             Search: (props) => <SearchIcon />,
             ResetSearch: (props) => <DeleteIcon />
           }}
-         
+
           components={{
             Action: (props) => (
               <Button
